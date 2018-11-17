facf8a28f2ce107fb728379ca8289c244924282f
aleksey <aleksey.konst@gmail.com>
2018-11-17 15:11:36 +0300

    Hibernate interceptor must analize statemens - it's not received as onChange and so on events

--- wfe-core/src/main/java/ru/runa/wfe/commons/hibernate/CacheInterceptor.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/hibernate/CacheInterceptor.java
@@ -1,18 +1,18 @@
 /*
  * This file is part of the RUNA WFE project.
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
- * as published by the Free Software Foundation; version 2.1 
- * of the License. 
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU Lesser General Public License for more details. 
- * 
- * You should have received a copy of the GNU Lesser General Public License 
- * along with this program; if not, write to the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 package ru.runa.wfe.commons.hibernate;
@@ -69,5 +69,14 @@ public class CacheInterceptor extends EmptyInterceptor {
     @Override
     public boolean onFlushDirty(Object entity, Serializable id, Object[] state, Object[] previousState, String[] propertyNames, Type[] types) {
         return onChanges(entity, Change.UPDATE, state, previousState, propertyNames, types, true);
+    }
+
+    @Override
+    public String onPrepareStatement(String sql) {
+        if (sql.toLowerCase().contains("delete")) {
+            ru.runa.wfe.commons.cache.sm.CachingLogic.resetAllCaches();
+        }
+        // TODO Auto-generated method stub
+        return super.onPrepareStatement(sql);
     }
 }
