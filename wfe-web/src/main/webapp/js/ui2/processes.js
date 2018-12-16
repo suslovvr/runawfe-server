c9ec96ae3fab36e16d49551ddfc8ce9e4bb20876
samonachowa <33526138+samonachowa@users.noreply.github.com>
2018-12-16 03:24:46 +0300

    Update processes.js

--- wfe-web/src/main/webapp/js/ui2/processes.js
+++ wfe-web/src/main/webapp/js/ui2/processes.js
@@ -7,6 +7,31 @@ wfe.processes = new function() {
                 el: "#spa-body",
                 data: data
             });
+            var c_height = $(window).height()-180;
+			$(".one-contentback").css('minHeight', c_height);
+			$(".task tr:nth-child(2) td p").append("<img src='img/ico/top-tun.png' class='filter-ico' />");
+            $("#but-filter").click(function() {
+                $("#view").hide();
+                $("#info").hide();
+                $(".filter").toggleClass("vert-mid");
+                $(".filter-ico").toggle();
+                $(".task tr:nth-child(1)").toggleClass("light1");
+                $(".task tr:nth-child(3)").toggleClass("light1");
+                $(".task tr:nth-child(2) td").toggleClass("light1 vert-line");
+                if(c_height < oc_height){$(".filter").hasClass("vert-mid") ? $(".one-contentback").height($(".one-contentback").height()+130): $(".one-contentback").height($(".one-contentback").height()-130);}
+            })
+			$(".close-ti").click(function() {
+                $("#view").hide();
+				$("#info").hide();
+            })
+            $("#but-view").click(function() {
+               $("#view").toggle();
+               $("#info").css("display","none");
+            })
+            $("#but-info").click(function() {
+               $("#info").toggle();
+               $("#view").css("display","none");
+            })
         });
     };
      this.onUnload = function() {
